1f8e0a4e33449d5d71a2abe84617be66de8afa38
Jakob Buis <jakob@jakobbuis.nl>
2019-08-10 22:38:10 +0200

    add a --force flag that overrides the timed trigger

--- wakeup.php
+++ wakeup.php
@@ -5,18 +5,23 @@ use WakeupLight\WakeupProgramme;
 use Yeelight\Bulb\Bulb;
 use Yeelight\YeelightClient;
 
+// passing --force as the first argument, overrides the timed trigger
+$forced = (isset($argv[1]) && $argv[1] ===  '--force');
+
 // This script is executed every minute by cron. The actual start time is defined
 // in ./trigger. Check that timestamp and fire the script if it matches
-$trigger = @file_get_contents(__DIR__ . '/trigger');
-if ($trigger === false) {
-    fwrite(STDERR, "No trigger file specified\n");
-    exit(0);
-}
-$trigger = trim($trigger);
-$now = date('H:i');
-if ($trigger !== $now) {
-    fwrite(STDERR, "Now {$now} is not equal to defined trigger {$trigger}, not firing.\n");
-    exit(0);
+if (!$forced) {
+    $trigger = @file_get_contents(__DIR__ . '/trigger');
+    if ($trigger === false) {
+        fwrite(STDERR, "No trigger file specified\n");
+        exit(0);
+    }
+    $trigger = trim($trigger);
+    $now = date('H:i');
+    if ($trigger !== $now) {
+        fwrite(STDERR, "Now {$now} is not equal to defined trigger {$trigger}, not firing.\n");
+        exit(0);
+    }
 }
 
 // Bootstrap system
